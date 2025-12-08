ff4669a70d1b0b9b669e3887ca058420e0133449
Enkhjin Ganaa <49260986+Enkhjin1215@users.noreply.github.com>
2025-12-08 14:23:52 +0800

    changes

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,24 +1,9 @@
-"""
-URL configuration for myproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from app.views import LotteryViewSet
+from app.views import LotteryViewSet, LotteryByPhoneView
+
 
 router = DefaultRouter()
 router.register(r'lotteries', LotteryViewSet, basename='lottery')
@@ -30,6 +15,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('by-phone/', LotteryByPhoneView.as_view(), name='lottery-by-phone'),
+
 ]
 
 # MEDIA файлуудыг serve хийх тохиргоо
